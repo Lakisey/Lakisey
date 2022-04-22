abcd12d3b3e7f865a5b1bb23f645439ba5aaea76
DESKTOP-RTGSJPP\alexs <alexsey100@gmail.com>
2022-04-22 13:36:06 +0300

    first commit

---
+++ let.js
@@ -0,0 +1,6 @@
+﻿"use strict"
+let admin;
+let name;
+  name = 'Jhon'
+  admin = name
+alert( `Hello ${admin}` );//заебись код 
---
+++ prompt.js
@@ -0,0 +1,3 @@
+﻿"use strict"
+let username = prompt("What is your name?","");
+alert (`Fuck you, ${username}`) ; //заебись код 
